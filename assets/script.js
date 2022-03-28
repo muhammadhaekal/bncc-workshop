1239bbba2787f680f32ddee51b221a02568089f1
Clarissa <clarissa.aprilliana@gmail.com>
2022-03-28 17:38:50 +0700

    9. JS - DOM Manipulation on click

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,32 @@ window.addEventListener('load', function() {
 function handleClose() {
   document.getElementById('modal').style.display = 'none';
 }
+
+const listSkills = [
+  {
+    title: 'Programming',
+    image: 'assets/img/image-1.jpeg',
+    detail: 'About Programming',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ab numquam quisquam veritatis minima iusto!'
+  },
+  {
+    title: 'Communication',
+    image: 'assets/img/image-2.jpeg',
+    detail: 'About Communication',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ab numquam quisquam veritatis minima iusto!'
+  },
+  {
+    title: 'Design',
+    image: 'assets/img/image-3.jpeg',
+    detail: 'About Design',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ab numquam quisquam veritatis minima iusto!'
+  },
+];
+
+function handleClick(index) {
+  document.getElementById('modal').style.display = 'block';
+  document.getElementById('title').innerHTML = listSkills[index].title;
+  document.getElementById('image').innerHTML = `<img src="${listSkills[index].image}" alt="img_${index}" width=200/>`;
+  document.getElementById('detail').innerHTML = listSkills[index].detail;
+  document.getElementById('description').innerHTML = listSkills[index].description;
+}
