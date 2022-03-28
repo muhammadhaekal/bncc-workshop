06f830db9e5c89f3632da146aeb9207477e9641d
Clarissa <clarissa.aprilliana@gmail.com>
2022-03-28 17:16:48 +0700

    8. JS - DOM Manipulation on load

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
 /**
  * Start of script file
  */
+window.addEventListener('load', function() {
+  document.getElementById('modal').style.display = 'block';
+  document.getElementById('title').innerHTML = 'Hello World!';
+  document.getElementById('detail').innerHTML = 'Welcome to My Personal Website';
+  document.getElementById('description').innerHTML = 'Here you can see my latest project and portfolio';
+});
+
+function handleClose() {
+  document.getElementById('modal').style.display = 'none';
+}
